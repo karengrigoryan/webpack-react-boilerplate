97e3be075bd3b8fa63069b8ae68ba4218a38dd14
Karen Grigoryan <karen.grigoryan9@gmail.com>
2019-04-07 16:20:10 +0400

    Add dependencies, create App component, define webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+};
